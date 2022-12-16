4485ea8360ee07fa70cf8e772d3d211aecc02928
rama hasiba <ramahaseeba@gmail.com>
2022-12-16 14:07:43 +0300

    first commit

---
+++ src/utilis.py
@@ -0,0 +1,74 @@
+import json
+#get retreived docs from elasticsearch 
+def retreived_docs(query_fields, es_connection):
+    """
+    """
+    documents = es_connection.search(
+        body = { 
+            "size": 1000, 
+            "query": {
+                "bool": {
+                    "must":
+                    [
+                        {
+                            "fuzzy":{
+                                "text":{
+                                    "value": query_fields['text'],
+                                    "fuzziness": "AUTO",
+                                }
+                            }
+                        }, 
+                        {
+                            "range": {
+                                "created_at": {
+                                    "gte": query_fields['date_gte'],
+                                    "lte": query_fields['date_lte'] 
+                                }
+                            }
+                        },      
+                        {
+                            "geo_distance": {
+                                "distance": (str(query_fields['distance'])+ "km"),       
+                                "coordinates": 
+                                    [
+                                        float(query_fields['lng']),
+                                        float(query_fields['lat'])
+                                    ] 
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+    )     
+    return documents
+ 
+#get the elasticsearch response and return the coordinates matrix 
+def get_coordinates_list(es_response):
+    """
+    a function to take an elasticsearch response and return 
+    dictionaries list each dictionary contains the lat, lng, and score for 
+    the given document 
+    """
+    #define coordinates_score_list which will take the coordinates for each index 
+    coordinates_score_list = []
+    #a loop to get all coordinates
+    for index in range(len(es_response["hits"]['hits'])):  
+
+        coordinates_dict = {}            
+        coordinates = es_response["hits"]['hits'][index]['_source']['coordinates']['coordinates'] 
+        coordinates_dict["score"] = es_response["hits"]['hits'][index]['_score'] 
+        coordinates_dict["lat"] = coordinates[1]
+        coordinates_dict["lng"] = coordinates[0]
+        coordinates_score_list.append(coordinates_dict)
+        index = index + 1 
+    # [lng, lat] 
+    return coordinates_score_list 
+
+def write_respone_data(coordinates):
+    """
+    write the coordinates and score list to the json file 
+    """
+    with open('./static/mapData/data.json', 'w') as file:
+        json.dump(coordinates, file)
+
---
+++ static/js/main.js
@@ -0,0 +1,84 @@
+window.onload = async function () {};
+
+var baseLayer = L.tileLayer(
+  "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    attribution: "...",
+    maxZoom: 20,
+  }
+);
+var cfg = {
+  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+  // if scaleRadius is false it will be the constant radius used in pixels
+  radius: 40,
+  maxOpacity: 1,
+  // scales the radius based on map zoom
+  scaleRadius: false,
+  // if set to false the heatmap uses the global maximum for colorization
+  // if activated: uses the data maximum within the current map boundaries
+  //   (there will always be a red spot with useLocalExtremas true)
+  useLocalExtrema: true,
+  // which field name in your data represents the latitude - default "lat"
+  latField: "lat",
+  // which field name in your data represents the longitude - default "lng"
+  lngField: "lng",
+  // which field name in your data represents the data value - default "value"
+  valueField: "score",
+};
+
+var heatmapLayer = new HeatmapOverlay(cfg);
+
+var map = new L.Map("map-canvas", {
+  center: new L.LatLng(25.6586, -80.3568),
+  zoom: 2,
+  layers: [baseLayer, heatmapLayer],
+});
+let data = [];
+heatmapLayer.setData(
+  (testData = {
+    data: data,
+  })
+);
+/*New heat end*/
+
+function getFormData() {
+  let formData = {
+    text: $("#text")[0].value,
+    date_gte: $("#date_gte")[0].value,
+    date_lte: $("#date_lte")[0].value,
+    lat: $("#lat")[0].value,
+    lng: $("#lng")[0].value,
+    distance: $("#distance")[0].value,
+  };
+  return formData;
+}
+
+async function getPoints() {
+  let formData = getFormData();
+  let points = await fetch(
+    "/search?text=" +
+      formData.text +
+      "&date_gte=" +
+      formData.date_gte +
+      "&date_lte=" +
+      formData.date_lte +
+      "&lat=" +
+      formData.lat +
+      "&lng=" +
+      formData.lng +
+      "&distance=" +
+      formData.distance
+  ).then(async (response) => {
+    res = await response.json(); 
+    data = res.coordinates
+    heatmapLayer.setData(
+      (testData = {
+        data: data,
+      })
+    );     
+  });
+}
+
+$(document).ready(async function () {
+  $("#searchButton")[0].addEventListener("click", getPoints);
+});
